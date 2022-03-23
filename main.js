d6db4e71c6ac0e5ae8855d22345bda4178ab46fa
MisungChoi <choi.musing@gmail.com>
2022-03-23 15:38:14 +0900

    오디오 뮤트 추가

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 const video =  document.getElementById("myVideo");
-const btn = document.getElementById("myBtn")
+const audio = document.getElementById("myAudio");
+const btn = document.getElementById("myBtn");
 
 btn.addEventListener('click', myFunction);
 
 function myFunction() {
     if(video.paused) {
         video.play();
+        audio.play();
         btn.innerHTML = "Pause";
     } else {
         video.pause();
+        audio.pause();
         btn.innerHTML = "Play";
     }
 }
 
+const mute = document.getElementById("slient");
+mute.addEventListener('click', m);
+
+function m() {
+    if(audio.muted) {
+        audio.muted = false; 
+        mute.innerHTML = "Mute";    
+    }
+    
+    else {
+        audio.muted = true;
+        mute.innerHTML = "Sound play";  
+      }
+}
 
 
 const linkBtn = document.getElementById("naver");
